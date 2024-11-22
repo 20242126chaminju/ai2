6be80cab7348b6d06373e5f69513f5ac5da8159c
20242126chaminju <20242126@sdhs.gwe.hs.kr>
2024-11-22 15:08:35 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -88,7 +88,7 @@ content_data = {
             "https://www.youtube.com/watch?v=3JZ_D3ELwOQ"
         ],
         'texts': [
-            "성찬 느좋좋",
+            "성찬 느좋",
             "Label 1 관련 두 번째 텍스트 내용입니다.",
             "Label 1 관련 세 번째 텍스트 내용입니다."
         ]
